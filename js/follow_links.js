398f76d1a2879a6d8f2efefc53253b2d5c983b20
Empty-Fox <ulyana250188@gmail.com>
2020-07-07 16:54:12 +0300

    first commit

---
+++ js/follow_links.js
@@ -0,0 +1,57 @@
+function OpenLink1() {
+  var win = window.open('https://www.cyberghostvpn.com/ru_RU/offer/flash-devices?aff_id=1016&coupon=3yEuDo&aff_sub4=3yEuDo&source=pop&brand=top10bestvpn&aff_sub2=cVU15FmGEQ&utm_medium=affiliate&utm_source=1016', '_blank');
+  win.focus();
+}
+function OpenLink2() {
+  var win = window.open('https://nordvpn.com/uk/special/dark/?utm_medium=affiliate&utm_term&utm_content=AFF1&utm_campaign=off105&utm_source=aff508&utm_expid=.YLmgS-ugTw6AVn9loVnYvw.2&utm_referrer=https%3A%2F%2Fwww.top10bestvpn.com%2Fvisit%3Fproduct_id%3D2965%26url%3Dhttps%253A%252F%252Fgo.nordvpn.net%252Faff_c%253Faff_id%253D508%2526offer_id%253D105%2526url_id%253D954%2526aff_sub%253DAFF1%2526aff_sub2%253D%255Btracking-subid%255D%26rank%3D2%26cms_site%3D1%26d%3Dwww.top10bestvpn.com%26ignore_clickout%3Dtrue%26comp_id%3D5aaa70b4cd86c200113f574c%26comp_iid%3Dea44e45d9d8e196d9d1ee0eb79d39b18%26bi%3D%257B%2522pl%2522%253A11141%252C%2522p%2522%253A2965%252C%2522plr%2522%253A1504224%252C%2522site_id%2522%253A%252289%2522%252C%2522blrs%2522%253A51572%252C%2522plt%2522%253A%2522Default%2522%257D%26uid%3DsYiKdOb4AFVjOd426278%26riid%3D15H8gWk3puj6dq904baa%26gid%3D1935716998.1574177605', '_blank');
+  win.focus();
+}
+function OpenLink3() {
+  var win = window.open('https://surfshark.com/deals?aff=top10bestvpn&coupon=surfsharkdeal&transaction_id=102fd85fdf13725ee31d33ef88c75b&offer_id=97&affiliate_id=1353&source=&aff_sub=dgj9dIHX6Z&utm_source=Affiliates&utm_medium=1353&utm_campaign=affiliate&recurring_goal_id=86', '_blank');
+  win.focus();
+}
+function OpenLink4() {
+  var win = window.open('https://privatevpn.com', '_blank');
+  win.focus();
+}
+function OpenLink5() {
+  var win = window.open('https://www.ultravpn.com/6months-free-vpn-2', '_blank');
+  win.focus();
+}
+function OpenLink6() {
+  var win = window.open('https://offer.zenmate.com/ru/cliff?aff_id=1016&source=gen&coupon=175m&aff_sub2=zevvo6al81&utm_medium=affiliate&utm_source=1016', '_blank');
+  win.focus();
+}
+function OpenLink7() {
+  var win = window.open('https://fr.norton.com/products/norton-secure-vpn?SID=Wkbg2vTmMD&cjid=8300157&clickid=935447200d2111ea815000fb0a180513_707305212263920238%3Avnp1kyrXB_q_&af_sub4=aff&af_sub5=CJ&c=CJ&cjevent=935447200d2111ea815000fb0a180513', '_blank');
+  win.focus();
+}
+function OpenLink8() {
+  var win = window.open('https://www.pandasecurity.com/security-promotion/?reg=FR&campaign=dome1804&productID=VPNS&track=174033&utm_medium=affiliate&utm_source=PAP&utm_campaign=natint&utm_content=VPN#a_affid=natintvpn&a_bnid=ea2ff46e&data1=65qiDIh0ej', '_blank');
+  win.focus();
+}
+
+function ReadReview1() {
+  window.open('https://www.top10bestvpn.com/reviews/cyber-ghost', '_self');
+}
+function ReadReview2() {
+  window.open('https://www.top10bestvpn.com/reviews/nordvpn', '_self');
+}
+function ReadReview3() {
+  window.open('https://www.top10bestvpn.com/reviews/surfshark', '_self');
+}
+function ReadReview4() {
+  window.open('https://www.top10bestvpn.com/reviews/privatevpn', '_self');
+}
+function ReadReview5() {
+  window.open('https://www.top10bestvpn.com/reviews/ultravpn', '_self');
+}
+function ReadReview6() {
+  window.open('https://www.top10bestvpn.com/reviews/zenmate', '_self');
+}
+function ReadReview7() {
+  window.open('https://www.top10bestvpn.com/reviews/nortonwifiprivacy', '_self');
+}
+function ReadReview8() {
+  window.open('https://www.top10bestvpn.com/reviews/panda', '_self');
+}
---
+++ js/random_count.js
@@ -0,0 +1,6 @@
+
+var number = Math.floor(Math.random() * (1150 - 950)) + 950;
+var randomNumber = document.getElementById('number_members_descktop')
+var randomNumberMobile = document.getElementById('number_members_mobile')
+randomNumber.innerHTML = number;
+randomNumberMobile.innerHTML = number;
